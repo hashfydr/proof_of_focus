29d0186f535bf99b919788956e0d8e2f26aeeb2d
Harshit Garg <harshitgarg2k6@gmail.com>
2026-02-01 00:03:25 +0530

    added gitignore and remove build artifacts

--- pof/target/debug/deps/pof-62f956b404bcb31f.d
+++
@@ -1,5 +0,0 @@
-/home/hash/projects/proof_of_focus/pof/target/debug/deps/pof-62f956b404bcb31f.d: src/main.rs
-
-/home/hash/projects/proof_of_focus/pof/target/debug/deps/pof-62f956b404bcb31f: src/main.rs
-
-src/main.rs:
--- pof/target/debug/pof.d
+++
@@ -1,1 +0,0 @@
-/home/hash/projects/proof_of_focus/pof/target/debug/pof: /home/hash/projects/proof_of_focus/pof/src/main.rs
